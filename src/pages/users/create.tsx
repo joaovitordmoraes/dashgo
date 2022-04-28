e64e061711f15e43ca3e18d1fbcd02ab009ea772
joaovitordmoraes <joaovitordmoraes@gmail.com>
2022-04-27 21:36:57 -0300

    Add navigation on links

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -11,6 +11,7 @@ import {
 import { Input } from 'components/Form/Input'
 import { Header } from 'components/Header'
 import { Sidebar } from 'components/Sidebar'
+import Link from 'next/link'
 
 export default function CreateUser() {
   return (
@@ -45,7 +46,11 @@ export default function CreateUser() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
